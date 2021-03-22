ddf8d73723e848a2bb7d29952e235ee0f956e14a
NekozillaXD <45684444+NekozillaXD@users.noreply.github.com>
2021-03-22 21:19:45 +0300

    Added ModInverse, Modexp and Decryptor.cs

---
+++ Decryptor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSA
+{
+    class Decryptor
+    {
+        public BigInteger Decrypt(BigInteger P, BigInteger Q, int E, BigInteger c)
+        {
+            var d = MathUtils.ModInverse(E, (P - 1) * (Q - 1));
+            var n = P * Q;
+
+            var M = MathUtils.ModExp(c, d, n);
+            return M;
+        }
+    }
+}
